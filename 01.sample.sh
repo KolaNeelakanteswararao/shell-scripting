3cf84abd2e4efa39d23f2233f8d665fe8d441517
KolaNeelakanteswararao <neelakanta165@gmail.com>
2022-12-10 22:16:33 +0530

    colors,line and tab space opearations

--- 01.sample.sh
+++ 01.sample.sh
@@ -2,7 +2,7 @@
 
 echo Hello World!
 
-echo -e "\e[31mRed\e[32mYellow\e[34mBlue\e[35mMagenta\[36mCyan\e[0m"
+echo -e "\e[31mRed\e[32mYellow\e[34mBlue\e[35mMagenta\e[36mCyan\e[0m"
 
 echo -e "\e[1;31mRed\e[1;32mYellow\e[1;34mBlue\e[1;35mMagenta\e[1;36mCyan\e[0m"
 
