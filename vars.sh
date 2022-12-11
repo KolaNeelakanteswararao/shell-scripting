a7f87a11d72227576543282835f3bc525ff662bb
KolaNeelakanteswararao <neelakanta165@gmail.com>
2022-12-11 19:38:20 +0530

    variables

---
+++ vars.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+student_name="Raju"
+echo student_name=$student_name
+echo student_name=${student_name}
+
+Date=11-12-2022
+echo Good Morning Today date is $Date
+
+#DATE=$(date +%f)
+#echo Good Morning Today date is $DATE
+
+expr=$((2+3-4*5/7*8))
+echo expr output=$expr
