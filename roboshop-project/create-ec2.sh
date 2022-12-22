e92b6e1c50138930a58f2ecfc5e35801647a45e3
neela <neelakanta165@gmail.com>
2022-12-22 13:15:37 +0530

    updating .

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -13,14 +13,32 @@ if [ -z "${AMI_ID}" ];then
    echo -e "\e[1;31mUnable to find image  AMI_ID\e[0m"
    exit
 else
-  echo -e "\e[1;32mAMI_ID=${AMI_ID}\e[0m"
+   echo -e "\e[1;32mAMI_ID=${AMI_ID}\e[0m"
 fi
 
 PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME} --query 'Reservations[*].Instance[*].PrivateIpAddress' --output text)
 
 if [ -z "${PRIVATE_IP}" ]; then
-  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+  SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all --query "SecurityGroups[*].GroupId" --output text)
+  if [ -z "${SG_ID}" ]; then
+    echo -e "\e[1;33m Security group allow-all does not exist"
+    exit
+  fi
+  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids
 else
   echo "Instance ${INSTANCE_NAME} is already exits,Hence not creating"
-  exit
 fi
+
+IPADDRESS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME} --query 'Reservations[*].Instance[*].PrivateIpAddress' --output text)
+
+echo '{
+                  "Comment": "CREATE/DELETE/UPSERT a record ",
+                  "Changes": [{
+                  "Action": "UPSERT",
+                              "ResourceRecordSet": {
+                                          "Name": "DNSNAME",
+                                          "Type": "A",
+                                          "TTL": 300,
+                                       "ResourceRecords": [{ "Value": "IPADDRESS"}]
+      }}]
+      }' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" >/tmp/record.json
