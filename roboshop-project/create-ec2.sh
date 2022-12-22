a932c5a6fa1cd3069b716a5ee1695ce9412efd45
neela <neelakanta165@gmail.com>
2022-12-22 16:07:21 +0530

    updating .

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -2,7 +2,7 @@
 
 INSTANCE_NAME=$1
 
-if [ -z "$INSTANCE_NAME" ]; then
+if [ -z "${INSTANCE_NAME}" ]; then
   echo "\e[1;33mInstance Name Arguement is needed\e[0m"
   exit
 fi
@@ -21,7 +21,7 @@ PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANC
 if [ -z "${PRIVATE_IP}" ]; then
   SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all --query "SecurityGroups[*].GroupId" --output text)
   if [ -z "${SG_ID}" ]; then
-    echo -e "\e[1;33m Security group allow-all does not exist"
+    echo -e "\e[1;33m Security group allow-all does not exist\e[0m"
     exit
   fi
   aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids
