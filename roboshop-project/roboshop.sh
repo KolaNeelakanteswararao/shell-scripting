34cfae93104df69153da21d6e2fa910bbe636f33
neela <neelakanta165@gmail.com>
2022-12-22 21:20:40 +0530

    updating .

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-bash components/$1.sh
+if [ -f components/$1.sh ]; then
+  bash components/$1.sh
+else
+  echo -e "\e[1;31m Invalid inputs\e[0m"
+  echo -e "\e[1;32mAvailable Inputs frontend|cart|catalogue|dispatch|mongodb|mysql|payment|rabbitmq|redis|shipping|user\e[0m"
+fi
