ec3af33357d74b531c549f448f0db9154ccf49c1
neela <neelakanta165@gmail.com>
2022-12-23 17:00:35 +0530

    updating .

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -1,10 +1,10 @@
 source components/common.sh
 
-echo "set up mangodb repo file"
+echo "set up mongodb repo file"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 STAT $?
 
-echo "Installing Mangodb"
+echo "Installing Mongodb"
 yum install mongodb-org -y &>>$LOG_FILE
 STAT $?
 
@@ -12,9 +12,9 @@ echo "updating config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
 STAT $?
 
-echo "start the mangodb service"
-systemctl enable mangod &>>$LOG_FILE
-systemctl start mangod &>>$LOG_FILE
+echo "start the mongodb service"
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
 STAT $?
 
 echo "download the schema"
