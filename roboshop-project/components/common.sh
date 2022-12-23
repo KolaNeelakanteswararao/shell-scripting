85bb99274f8ba6c40b37f7788d734cf03556c3e7
neela <neelakanta165@gmail.com>
2022-12-23 16:21:33 +0530

    updating .

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -1,3 +1,12 @@
 LOG_FILE=/tmp/roboshop.log
 
 rm -f $LOG_FILE
+
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32m Success\e[0m"
+  else
+    echo -e "\e[1;31m Failed\e[0m"
+    exit
+  fi
+}
