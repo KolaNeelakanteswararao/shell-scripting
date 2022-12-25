c58a5941071824b570719be66737a3a7137b7f72
neela <neelakanta165@gmail.com>
2022-12-25 17:30:50 +0530

    updating .

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -43,7 +43,7 @@ SYSTEMD_SETUP() {
     chown roboshop:roboshop /home/roboshop -R
 
     echo "update systemD file"
-    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
     STAT $?
 
     echo "setup ${COMPONENT} systemD file"
@@ -89,6 +89,30 @@ JAVA() {
   cd /home/roboshop/${COMPONENT}
   mvn clean package &>>$LOG_FILE
   mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
+  STAT $?
+
+  SYSTEMD_SETUP
+
+}
+
+PYTHON() {
+  COMPONENT=$1
+
+  echo "Installing PYTHON"
+  yum install python36 gcc python3-devel -y &>>$LOG_FILE
+  STAT $?
+
+  APP_USER_SETUP_WITH_APP
+
+  echo "Install Python Dependencies for ${COMPONENT}"
+  cd /home/roboshop/{COMPONENT}
+  pip3 install -r requirements.txt &>>$LOG_FILE
+  STAT $?
+
+  echo "Update Application Config"
+  USER_ID=$(id -u roboshop)
+  GROUP_ID=$(id -g roboshop)
+  sed -i -e "/uid/ c uid=${USER_ID}" -e "/gid/ c gid=${GROUP_ID}" /home/roboshop/${COMPONENT}/${COMPONENT}.ini
   STAT $?
 
   SYSTEMD_SETUP
---
+++ roboshop-project/components/rabbitmq.sh
@@ -0,0 +1,28 @@
+source components/common.sh
+
+echo "Configure YUM repo"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>$LOG_FILE
+yum install erlang -y &>>$LOG_FILE
+STAT $?
+
+echo "Install RabbitMQ Server"
+yum install rabbitmq-server -y &>>$LOG_FILE
+STAT $?
+
+echo "Start RabbitMQ server"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl start rabbitmq-server &>>$LOG_FILE
+STAT $?
+
+echo "Create application user"
+rabbitmqctl list_user | grep roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+fi
+STAT $?
+
+echo "Setup permission for App user"
+rabbitmqctl set_user_tags roboshop administrator &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+STAT $?
